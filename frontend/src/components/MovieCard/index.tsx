33185785a09b61bfe393327d7297b15dd62c4fc1
Gilnei Gonzo <gilbegprog@gmail.com>
2022-01-11 14:04:18 -0300

    Navegation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import MovieScore from '../MovieScore';
 import MovieStars from '../MovieStars';
 import './styles.css';
@@ -18,8 +19,11 @@ function MovieCard() {
       <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
         <div className="dsmovie-card-bottom-container">
             <h3>{movie.title}</h3>
-            <MovieScore />
-            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+              <MovieScore />
+
+               <Link to={`/form/${movie.id}`}>
+                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+               </Link>            
         </div>
     </div>
   );
